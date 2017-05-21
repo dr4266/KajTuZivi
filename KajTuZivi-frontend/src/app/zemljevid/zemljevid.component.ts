bb706cb4686d0937777d256508fc6b7a8eec8629
Blaz <blaz.vrhovsek95@gmail.com>
2017-05-21 02:31:19 +0200

    Geolocation

--- KajTuZivi-frontend/src/app/zemljevid/zemljevid.component.ts
+++ KajTuZivi-frontend/src/app/zemljevid/zemljevid.component.ts
@@ -12,11 +12,18 @@ import 'rxjs/add/operator/map';
 export class ZemljevidComponent implements OnInit {
 
   stateCtrl: FormControl;
-
-  lat = 51.678418;
-  lng = 7.809007;
+  public pos: any;
+  lat = 20.678418;
+  lng = 10.809007;
 
   ngOnInit() {
-  }
+  navigator.geolocation.getCurrentPosition((position) => {
+      this.lat=position.coords.latitude;
+      this.lng=position.coords.longitude;
+    });
+
+
+}
+
 
 }
