032a3fa8b3f660f856d4deeb8a8ef429f571a95b
Blaz <blaz.vrhovsek95@gmail.com>
2017-05-21 05:15:51 +0200

    Povezave

---
+++ KajTuZivi-frontend/src/app/shared/services/podatki.services.ts
@@ -0,0 +1,13 @@
+import {Injectable} from '@angular/core'
+import {Http, Response} from '@angular/http'
+import {Observable} from 'rxjs/Observable'
+import {Config} from '../config/env.config'
+
+@Injectable ()
+export class PodatkiService{
+	constructor(private http:Http){}
+
+	vstaviPopis(podatki:any):Observable<any>{
+		return this.http.post(Config.API+ 'popisi/', podatki).map((res:Response)=>res.json()); 
+	}
+}
